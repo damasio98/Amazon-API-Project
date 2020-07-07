3cdda8dbdf0e789ac02785e12b44a07c9b959993
damasio98 <jdpft17@gmail.com>
2020-07-07 16:43:09 +0100

    commit

--- src/parametros.py
+++ src/parametros.py
@@ -1,8 +1,8 @@
 ASIN = "B01MQPT9OD"  #pode ser encontrado no link do produto 
 país = 'ES'          #outros dominios: "COM" "IT" "GB" "DE" etc
 
-preço_atual = 100    #necessário para construir a variável diferença
-preço_ideal = 80
+preço_atual = 55    #necessário para construir a variável diferença
+preço_ideal = 40
 
 
 
